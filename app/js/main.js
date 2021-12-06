649e33a0d09b45235605fc35bc940033213ca8ab
Nazzzux <alex.rochnyak@gmail.com>
2021-12-06 23:02:49 +0200

    first page decstop done

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(function(){
 
+   $('.reviews__slider').slick({
+      dots: true,
+      prevArrow: '<button type="button" class="slick-btn slick-prev"><svg class="slick-svg" width="34" height="34"><use xlink:href="images/sprite/images.svg#prev_arrow"></svg></button>',
+      nextArrow: '<button type="button" class="slick-btn slick-next"><svg class="slick-svg" width="34" height="34"><use xlink:href="images/sprite/images.svg#next_arrow"></svg></button>',
+      slidesToShow: 1,
+   });
+
    var mixer = mixitup('.catalog__list');
 });
+
