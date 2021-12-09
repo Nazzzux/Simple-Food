3f43d27ef8119f1c76f9e75112c4a7a174b2f621
Nazzzux <alex.rochnyak@gmail.com>
2021-12-09 19:23:08 +0200

    all changes

--- app/js/main.js
+++ app/js/main.js
@@ -2,8 +2,8 @@ $(function(){
 
    $('.reviews__list').slick({
       dots: true,
-      prevArrow: '<button type="button" class="slick-btn slick-prev"><svg class="slick-svg" width="34" height="34"><span class="sr-only">Кнопка предыдущий слайд</span><use xlink:href="images/sprite/images.svg#prev_arrow"></svg></button>',
-      nextArrow: '<button type="button" class="slick-btn slick-next"><svg class="slick-svg" width="34" height="34"><span class="sr-only">Кнопка следующий слайд</span><use xlink:href="images/sprite/images.svg#next_arrow"></svg></button>',
+      prevArrow: '<button type="button" class="slick-btn slick-prev"><span class="sr-only">Кнопка предыдущий слайд</span><svg class="slick-svg" width="34" height="34"><use xlink:href="images/sprite/images.svg#prev_arrow"></svg></button>',
+      nextArrow: '<button type="button" class="slick-btn slick-next"><span class="sr-only">Кнопка следующий слайд</span><svg class="slick-svg" width="34" height="34"><use xlink:href="images/sprite/images.svg#next_arrow"></svg></button>',
       slidesToShow: 1,
    });
 
