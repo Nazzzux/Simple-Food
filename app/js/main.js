d707b856e21626fdb219f34a481858770417b4f0
Nazzzux <alex.rochnyak@gmail.com>
2021-12-09 15:26:05 +0200

    before changing list in footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-   $('.reviews__slider').slick({
+   $('.reviews__list').slick({
       dots: true,
       prevArrow: '<button type="button" class="slick-btn slick-prev"><svg class="slick-svg" width="34" height="34"><use xlink:href="images/sprite/images.svg#prev_arrow"></svg></button>',
       nextArrow: '<button type="button" class="slick-btn slick-next"><svg class="slick-svg" width="34" height="34"><use xlink:href="images/sprite/images.svg#next_arrow"></svg></button>',
