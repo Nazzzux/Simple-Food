914308889f6cdf68b2fd14f7475b56480466513b
Nazzzux <alex.rochnyak@gmail.com>
2022-01-04 15:38:37 +0200

    working swiper

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,37 @@
 $(function () {
 
+   // swiper
 
+   var swiper = new Swiper('.reviews__list', {
+		slidesPerView: 1,
+		spaceBetween: 20,
+		loop: true,
 
-   $('.reviews__list').slick({
-      dots: true,
-      prevArrow: '<button type="button" class="slick-btn slick-prev"><span class="sr-only">Кнопка предыдущий слайд</span><svg class="slick-svg" width="34" height="34"><use xlink:href="images/sprite/images.svg#prev_arrow"></svg></button>',
-      nextArrow: '<button type="button" class="slick-btn slick-next"><span class="sr-only">Кнопка следующий слайд</span><svg class="slick-svg" width="34" height="34"><use xlink:href="images/sprite/images.svg#next_arrow"></svg></button>',
-      slidesToShow: 1,
+		// pagination: {
+		// 	el: ".dots--top",
+		// 	clickable: true,
+		// },
+		navigation: {
+      	clickable: true,
+			nextEl: '.swiper-button-prev',
+			prevEl: '.swiper-button-next',
+		}
    });
+   // swiper
+
+
+   // slick-slider
+   
+   // $('.reviews__list').slick({
+   //    dots: true,
+   //    prevArrow: '<button type="button" class="slick-btn slick-prev"><span class="sr-only">Кнопка предыдущий слайд</span><svg class="slick-svg" width="34" height="34"><use xlink:href="images/sprite/images.svg#prev_arrow"></svg></button>',
+   //    nextArrow: '<button type="button" class="slick-btn slick-next"><span class="sr-only">Кнопка следующий слайд</span><svg class="slick-svg" width="34" height="34"><use xlink:href="images/sprite/images.svg#next_arrow"></svg></button>',
+   //    slidesToShow: 1,
+   // });
+
+   // slick-slider
+
+
 
    $('.filter-price__input').ionRangeSlider({
       type: 'double',
--- gulpfile.js
+++ gulpfile.js
@@ -41,9 +41,10 @@ function styles() {
 function scripts() {
    return src([
       'node_modules/jquery/dist/jquery.js',
-      'node_modules/slick-carousel/slick/slick.js',
+      // 'node_modules/slick-carousel/slick/slick.js',
       'node_modules/mixitup/dist/mixitup.js',
       'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+      'node_modules/swiper/swiper-bundle.js',
       'app/js/main.js'
    ])
    .pipe(concat('main.min.js'))
